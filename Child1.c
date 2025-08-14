11fe7e2f432314f22c9e3e1f612b863bd1e219a4
Sushi674244122 <674244122@mail.pbru.ac.th>
2025-08-14 12:06:01 +0700

    Add files via upload

---
+++ Child1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main() {
+    printf("My bio.: \n");
+    printf("ID : 654244001 \n");
+    printf("Name : Puwadon Changroeng\n");
+    printf("------------------------------\n");
+}
